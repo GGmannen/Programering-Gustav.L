d6eba4527a4a8edfef57d58cc82ff134b9bb9d47
GGmannen <gustav.loof@elev.ga.ntig.se>
2019-09-30 11:06:37 +0200

    Kattis läxa 1

---
+++ Hemläxor/v39/Kattis_R2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Kattis_R2 {
+
+	public static void main(String[]args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Skriv in värde för S");
+		int s = input.nextInt();
+		
+		System.out.println("Skriv in Värde för R1");
+		int r1 = input.nextInt();
+		
+		System.out.println(2*s-r1);
+		
+
+		
+	
+	}
+	
+	
+}
