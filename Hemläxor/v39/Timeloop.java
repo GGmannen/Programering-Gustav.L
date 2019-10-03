1e56ee6155ca7aa5140ef554507283ac9ee49583
GGmannen <gustav.loof@elev.ga.ntig.se>
2019-10-03 10:25:34 +0200

    Gjorde en Timeloop

---
+++ Hemläxor/v39/Timeloop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Timeloop {
+
+	public static void main(String[]args) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Ange Värde");
+		int x = input.nextInt();
+		
+		
+		for (int i = 1; i <= x; i++) {
+			System.out.println(i +" \t " + "Abracadabra");
+		}
+		
+		}
+	}
+	
