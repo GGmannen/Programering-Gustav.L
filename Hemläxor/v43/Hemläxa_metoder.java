e885d437acef4bebd1593f17b77009e81b27169b
GGmannen <gustav.loof@elev.ga.ntig.se>
2019-10-24 10:32:47 +0200

    Skapade en input och output för volym

---
+++ Hemläxor/v43/Hemläxa_metoder.java
@@ -0,0 +1,24 @@
+
+public class Hemläxa_metoder {
+
+	
+	public static void main(String[]args) {
+	
+	
+		System.out.println(volume(5)+"liter");
+		
+		
+	}
+	
+	
+	double volume(double radius) {
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
