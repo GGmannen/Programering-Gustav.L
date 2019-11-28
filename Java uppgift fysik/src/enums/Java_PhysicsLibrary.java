8b7135a62d5ded717088c2a7dc9de62b101fd086
GGmannen <gustav.loof@elev.ga.ntig.se>
2019-11-28 10:52:11 +0100

    Har skrivit fler metoder med JavaDoc

--- Java uppgift fysik/src/enums/Java_PhysicsLibrary.java
+++ Java uppgift fysik/src/enums/Java_PhysicsLibrary.java
@@ -66,7 +66,7 @@ public class Java_PhysicsLibrary {
 		
 	}
 	/**
-	 * Metod för att räkna ut trycket i vattnet vid ett betämt djup
+	 * Metod för att räkna ut trycket i vätska vid ett betämt djup
 	 * @param fluid vätskans densitet
 	 * @param deep djupet i meter
 	 * @return skickar tillbaka det beräknade trycket
@@ -79,7 +79,11 @@ public class Java_PhysicsLibrary {
 		
 	}
 	
-	
+	/**
+	 * Räknar ut trycket under vattnet vid ett bestämt djup
+	 * @param deep djupet i vattnet
+	 * @return skickar tillbaka de beräknade trycket
+	 */
 	public static double pressureUnderWater(double deep) {
 		
 		double pressureUnder = FluidTable.WATER.density * g_swe * deep;
@@ -87,7 +91,12 @@ public class Java_PhysicsLibrary {
 		
 		
 	}
-	
+	/**
+	 * räknar ut kinetisk energi genom massan multiplicerat med hastigheten
+	 * @param mass massan för materialet
+	 * @param velocity hastigheten hos föremålet
+	 * @return skickar tillbaka värdet för den kinetiska energin
+	 */
 	public static double kineticEnergy(double mass, double velocity) {
 		
 		double kineticEnergy = (mass * Math.pow(velocity, 2)) / 2;
@@ -95,7 +104,12 @@ public class Java_PhysicsLibrary {
 		
 	}
 	
-	
+	/**
+	 * A method that calculates potential energy by multiplying mass, height and the gravitational pull in Sweden
+	 * @param mass The mass for a certain object
+	 * @param height The height the object has
+	 * @return sends back the potential energy which the object has
+	 */
 	public static double potentialEnergy(double mass, double height) {
 		
 		return mass * g_swe * height;
@@ -103,31 +117,55 @@ public class Java_PhysicsLibrary {
 		
 	}
 	
-	
+	/**
+	 * This method calculates the fall speed of a object which it can reach when getting dropped from a height
+	 * @param height the height the object is dropped from
+	 * @return Returns the calculated fall speed
+	 */
 	public static double fallSpeed(double height) {
 		
 		return Math.sqrt(2 * g_swe * height);
 				
 	}
-	
+	/**
+	 * Calculates the difference between two different values
+	 * @param first the predetermined first value
+	 * @param last the predetermined seccond value
+	 * @return
+	 */
 	public static double delta(double first, double last) {
 		
 		return last - first;
 		
 	}
-	
+	/**
+	 * A method that changes a volume of a fluid to a different mass
+	 * @param fluid the value of the fluid
+	 * @param volume the volume used when calculating the final value
+	 * @return Returns the mass that has been converted from volume to fluid
+	 */ 
 	public static double volumeToMass(FluidTable fluid, double volume) {
 		
 		return 	fluid.density * volume;
 		
 	}
-	
+	/**
+	 * This method converts the volume of a gas to a mass
+	 * @param gas the value of the gas
+	 * @param volume the volume of this gas
+	 * @return Returns the convertec volume in a mass
+	 */
 	public static double volumeToMassGas(GasTable gas, double volume) {
 		
 		return gas.density * volume;
 		
 	}
-	
+	/**
+	 * 
+	 * @param solid
+	 * @param volume
+	 * @return
+	 */
 	public static double volumeToMassSolid(SolidTable solid, double volume) {
 		
 		return solid.density * volume;
