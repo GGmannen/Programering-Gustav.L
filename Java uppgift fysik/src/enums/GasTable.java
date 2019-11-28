81d69ece9ac096cc6c1cd1879d8ea13352f495ff
GGmannen <gustav.loof@elev.ga.ntig.se>
2019-11-28 17:57:07 +0100

    Gas table och fluid table 2 tillagda värden

--- Java uppgift fysik/src/enums/GasTable.java
+++ Java uppgift fysik/src/enums/GasTable.java
@@ -4,7 +4,9 @@ public enum GasTable{
 
 	AIR(1.29, 1.01), 
 	O2(1.43, 0.92), 
-	OXYGEN(1.43, 0.92)
+	OXYGEN(1.43, 0.92),
+	HYDROGEN (0.09, 14.2),
+    CARBONDIOXIDE (1.98, 0.82)
 	;
 
 	double density;
