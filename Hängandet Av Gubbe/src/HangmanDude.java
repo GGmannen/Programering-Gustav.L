e8b6f1f131d150311c1edb43b17606644fc0fe0a
GGmannen <gustav.loof@elev.ga.ntig.se>
2020-05-14 17:38:04 +0200

    Har nu börjat på metoden där spelet körs och exprimenterar lite

--- Hängandet Av Gubbe/src/HangmanDude.java
+++ Hängandet Av Gubbe/src/HangmanDude.java
@@ -9,22 +9,22 @@ public class HangmanDude {
 	static int maxWrongs = 8;
 	static int wordCategory = 0;
 	static String playerGuess = "";
-	static String gameWord;
+	static String gameWord = "hejsan";
 	static Random rand = new Random();
 	static Scanner input = new Scanner(System.in);
 	static int playAgain;
-	
-	
+	static ArrayList<String> guessedLetters = new ArrayList<>();
+
 	public static void main(String[] args) {
-		welcomer();
+		gamePlayer();
 
 	}
 
 	public static void welcomer() {
-		System.out.println("Välkommen till ett spel som handlar om hängandet av en gubbe!");
-		System.out.println("Du kommer att få gissa på bokstäver i ord");
-		System.out.println("Gissar du fel så kommer gubben gradvis att hängas");
-		System.out.println("Du gissar på en bokstav i taget, gissar du rätt på ordet så vinner du spelet");
+		System.out.println("Welcome to a game about the hanging of a man!");
+		System.out.println("You are supposed to guess on letters in different words");
+		System.out.println("If your guess is wrong, the man will be hung");
+		System.out.println("You guess one letter at a time, if you guess the entire word correct you win");
 		System.out.println("GLHF!");
 		chooseWordCategory();
 
@@ -46,7 +46,7 @@ public class HangmanDude {
 			case 3:
 				Animals();
 			case 4:
-				
+
 			}
 		}
 	}
@@ -56,20 +56,19 @@ public class HangmanDude {
 		if (wordCategory == 1) {
 
 			ArrayList<String> cars = new ArrayList<String>();
-			cars.add("Volvo");
-			cars.add("Porsche");
-			cars.add("Mazda");
-			cars.add("Corvette");
-			cars.add("Koenigsegg");
-			cars.add("Bugatti");
-			cars.add("Mercedes");
-			cars.add("Lamborghini");
-			cars.add("Mclaren");
-			cars.add("Hennesey");
-			cars.add("Rimac");
-			cars.add("Maserati");
-			cars.add("Saab");
-			
+			cars.add("VOLVO");
+			cars.add("PORSCHE");
+			cars.add("MAZDA");
+			cars.add("CORVETTE");
+			cars.add("KOENIGSEGG");
+			cars.add("BUGATTI");
+			cars.add("MERCEDES");
+			cars.add("LAMBORGHINI");
+			cars.add("MCLAREN");
+			cars.add("HENNESEY");
+			cars.add("RIMAC");
+			cars.add("MASERATI");
+			cars.add("SAAB");
 
 			for (int i = 0; i < 1; i++) {
 				gameWord = (cars.get(random.nextInt(cars.size())));
@@ -84,17 +83,17 @@ public class HangmanDude {
 		if (wordCategory == 2) {
 
 			ArrayList<String> colors = new ArrayList<String>();
-			colors.add("Yellow");
-			colors.add("Pink");
-			colors.add("Blue");
-			colors.add("Purple");
-			colors.add("Green");
-			colors.add("Black");
-			colors.add("Brown");
-			colors.add("Orange");
-			colors.add("White");
-			colors.add("Red");
-			colors.add("Turquoise");
+			colors.add("YELLOW");
+			colors.add("PINK");
+			colors.add("BLUE");
+			colors.add("PURPLE");
+			colors.add("GREEN");
+			colors.add("BLACK");
+			colors.add("BROWN");
+			colors.add("ORANGE");
+			colors.add("WHITE");
+			colors.add("RED");
+			colors.add("TURQUOISE");
 
 			for (int i = 0; i < 1; i++) {
 				gameWord = (colors.get(random.nextInt(colors.size())));
@@ -109,22 +108,22 @@ public class HangmanDude {
 		if (wordCategory == 3) {
 
 			ArrayList<String> animals = new ArrayList<String>();
-			animals.add("Tiger");
-			animals.add("Panda");
-			animals.add("Cat");
-			animals.add("Dog");
-			animals.add("Parrot");
-			animals.add("Rabbit");
-			animals.add("Horse");
-			animals.add("Spider");
-			animals.add("Gorilla");
-			animals.add("Hamster");
-			animals.add("Pig");
-			animals.add("Cow");
-			animals.add("Elephant");
-			animals.add("Giraffe");
-			animals.add("Mouse");
-			animals.add("Rat");
+			animals.add("TIGER");
+			animals.add("PANDA");
+			animals.add("CAT");
+			animals.add("DOG");
+			animals.add("PARROT");
+			animals.add("RABBIT");
+			animals.add("HORSE");
+			animals.add("SPIDER");
+			animals.add("GORILLA");
+			animals.add("HAMSTER");
+			animals.add("PIG");
+			animals.add("COW");
+			animals.add("ELEPAHNT");
+			animals.add("GIRAFFE");
+			animals.add("MOUSE");
+			animals.add("RAT");
 
 			for (int i = 0; i < 1; i++) {
 				gameWord = (animals.get(random.nextInt(animals.size())));
@@ -133,38 +132,53 @@ public class HangmanDude {
 			gamePlayer();
 		}
 	}
-	
+
 	public static void gamePlayer() {
-																																																			
-		
-		
+
+		for (int i = 0; i < gameWord.length(); i++) {
+			System.out.print("_ ");
+		}
+
+		playerGuess = input.nextLine();
+		playerGuess = playerGuess.toUpperCase();
+
+		if (!guessedLetters.contains(playerGuess)) {
+
+			guessedLetters.add(playerGuess);
+
+			if (gameWord.contains(playerGuess)) {
+
+			}
+			
+			else {
+				amountWrongs ++;
+				
+			}
+
+		}
+
 	}
 
-	
 	public static void gameEnder() {
-		
-		System.out.println("Type: 1, To play again \n 2, to quit the game");
-		while(playAgain < 1 || playAgain > 2 ) {
-			
+
+		System.out.println("Type: 1, To play again \n 2, To quit the game");
+		while (playAgain < 1 || playAgain > 2) {
+
 			playAgain = input.nextInt();
-			
-			if(playAgain == 1) {
+
+			if (playAgain == 1) {
 				amountWrongs = 0;
 				wordCategory = 0;
 				gameWord = "";
 				playAgain = 0;
 				chooseWordCategory();
-			}
-			else if(playAgain == 2) {
+			} else if (playAgain == 2) {
 				System.exit(0);
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
-	
-	
+
 }
